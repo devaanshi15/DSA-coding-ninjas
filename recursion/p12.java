81cf0cec9b2ef0871c4e83fe4e38bbc55ea089bb
devaanshi chaturvedi <devaanshic1509@gmail.com>
2024-06-30 11:56:00 +0530

    recursion don TT

--- recursion/p12.java
+++ recursion/p12.java
@@ -1,20 +1,65 @@
+//merge sort
+
 public record p12() {
-    public static int binaryS(int a[], int si, int ei, int x){
-        if(si>ei)
-            return -1;
 
-        int mi= (si+ei)/2;
+    public static void merge(int s1[], int s2[], int finalA[]) {
+        int i = 0, j = 0, k = 0;
 
-        if(a[mi]==x)
-            return mi;
-        else if(x>a[mi])
-            return binaryS(a, mi+1, ei, x);
-        else
-            return binaryS(a, mi-1, ei, x);
+        while (i < s1.length && j < s2.length) {
+
+            // if s1's first element is <= s2's first element, then it's going in the final array
+            if (s1[i] <= s2[j]) {
+                finalA[k] = s1[i];
+                i++;
+            } else {
+                finalA[k] = s2[j];
+                j++;
+            }
+            k++;
+        }
+
+        // s1 array elements still left
+        while (i < s1.length) {
+            finalA[k] = s1[i];
+            i++;
+            k++;
+        }
+
+        // or s2 array elements still left
+        while (j < s2.length) {
+            finalA[k] = s2[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void mergeSort(int a[]) {
+        if (a.length <= 1)
+            return;
+        int b[] = new int[a.length / 2];
+        int c[] = new int[a.length - b.length];
+
+        // copying elements in b
+        for (int i = 0; i < a.length / 2; i++) {
+            b[i] = a[i];
+        }
+
+        // copying elements in c
+        for (int i = a.length / 2; i < a.length; i++) {
+            c[i - a.length / 2] = a[i];
+        }
+
+        mergeSort(b);
+        mergeSort(c);
+
+        merge(b, c, a);
     }
 
     public static void main(String[] args) {
-        int a[] = {1,2,3,4,4,10, 100,203};
-        System.out.println(binaryS(a, 0, a.length-1, 100));
+        int a[] = {10, 4, 5, 3, 1, 50, 100, 31};
+        mergeSort(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
     }
 }
---
+++ recursion/p14.java
@@ -0,0 +1,16 @@
+//tower of hanoi
+
+public class p14{
+    public static void towerHanoi(int n,char s,char h,char d){
+        if(n==1){
+            System.out.println("move 1st disk from "+s+" to "+d);
+            return;
+        }
+        towerHanoi(n-1,s,d,h);
+        System.out.println("move"+n+" th disk from "+s+" to "+d);
+        towerHanoi(n-1,h,s,d);
+    }
+    public static void main(String[] args) {
+		towerHanoi(2,'s','h','d');
+	}
+}
